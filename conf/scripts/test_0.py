8866e5b0ab5196b283b4510e31650d190c578874
229033891 <229033891@qq.com>
2024-10-07 13:03:04 +0800

    update new file:   conf/scripts/test_0.py modified:   src/migrations/migrate.py modified:   web-src/package-lock.json modified:   web-src/package.json

---
+++ conf/scripts/test_0.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    while True:  
+        # 提示用户输入内容
+        user_input = input("请输入一些内容（输入 '@' 以退出）: ")
+        
+        # 检查用户是否想要退出
+        if user_input == '@':
+            print("程序已退出。")
+            break
+        
+        # 打印用户输入的内容
+        print("你输入的是: " + user_input)
+
+# 运行主函数
+if __name__ == "__main__":
+    main()
