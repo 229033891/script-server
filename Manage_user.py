fd684e5840d73b9d3bc7acb565ff976c3c203494
229033891 <229033891@qq.com>
2024-10-24 14:45:08 +0800

    modified:   Manage_user.py 	modified:   src/auth/auth_htpasswd.py

--- Manage_user.py
+++ Manage_user.py
@@ -13,18 +13,17 @@ def input_with_timeout(prompt, timeout):
     thread.join(timeout)
     if thread.is_alive():
         print("\033[91m超时! 程序将自动退出。\033[0m")
-        os._exit(1)  # 超时立即退出程序
+        os._exit(1)
     return result[0]
 
 def find_script_server_dir(starting_dir):
-    current_dir = starting_dir
     while True:
-        if os.path.basename(current_dir) == 'script-server':
-            return current_dir
-        parent_dir = os.path.dirname(current_dir)
-        if parent_dir == current_dir:
+        if os.path.basename(starting_dir) == 'script-server':
+            return starting_dir
+        parent_dir = os.path.dirname(starting_dir)
+        if parent_dir == starting_dir:
             break
-        current_dir = parent_dir
+        starting_dir = parent_dir
     return None
 
 def get_htpasswd_file_path():
@@ -40,8 +39,8 @@ def get_htpasswd_file_path():
 def generate_random_password(length=12):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
-def modify_user_file(username, htpasswd_file, new_password=None, delete=False):
-    updated, deleted = False, False
+def modify_user_file(username, htpasswd_file, new_password=None):
+    updated = False
     try:
         with open(htpasswd_file, 'r') as file:
             lines = file.readlines()
@@ -50,17 +49,13 @@ def modify_user_file(username, htpasswd_file, new_password=None, delete=False):
             for line in lines:
                 stored_user, stored_password = line.strip().split(':', 1)
                 if stored_user == username:
-                    if delete:
-                        deleted = True
-                    else:
+                    if new_password:
                         file.write(f"{username}:{new_password}\n")
                         updated = True
                 else:
                     file.write(line)
 
-        if delete and deleted:
-            print(f"\033[92m用户 '{username}' 已被删除。\033[0m")
-        elif new_password and updated:
+        if new_password and updated:
             print(f"\033[92m用户 '{username}' 的密码已更新。\033[0m")
             print(f"新密码: {new_password}")
         elif not updated:
@@ -75,6 +70,18 @@ def add_user(username, password, htpasswd_file):
     print(f"\033[92m用户 {username} 添加成功。\033[0m")
     print(f"用户名: {username}\n密码: {password}")
 
+def user_exists(username, htpasswd_file):
+    try:
+        with open(htpasswd_file, 'r') as file:
+            for line in file:
+                stored_user, _ = line.strip().split(':', 1)
+                if stored_user == username:
+                    return True
+        return False
+    except Exception as e:
+        print(f"\033[91m发生错误: {e}\033[0m")
+        return False
+
 def verify_user_password(username, password, htpasswd_file):
     try:
         with open(htpasswd_file, 'r') as file:
@@ -88,14 +95,11 @@ def verify_user_password(username, password, htpasswd_file):
         return False
 
 def change_user_password(username, htpasswd_file, manual=True):
-    old_password = input_with_timeout("请输入旧密码: ", 60)
-    if old_password == "@" or not verify_user_password(username, old_password, htpasswd_file):
-        print("\033[91m旧密码错误或取消操作。\033[0m")
-        return
-
     if manual:
         while True:
             new_password = input_with_timeout("请输入新密码: ", 60)
+            if new_password == "@":
+                return
             if new_password == "@" or input_with_timeout("请再次输入新密码: ", 60) != new_password:
                 print("\033[91m密码错误或不匹配。\033[0m")
                 continue
@@ -113,10 +117,10 @@ def main():
 
     while True:
         print("\n菜单:")
-        print("1. 生成文件(勿动)")
+        print("1. 初始密码文件(勿动)")
         print("2. 添加用户")
-        print("3. 自动更改用户密码")
-        print("4. 手动更改用户密码")
+        print("3. 自动更改密码")
+        print("4. 手动更改密码")
         print("@. 退出")
         choice = input_with_timeout("请选择一个选项: ", 60)
 
@@ -133,9 +137,21 @@ def main():
                 add_user(username, generate_random_password(), htpasswd_file)
 
         elif choice in ['3', '4']:
-            username = input_with_timeout("请输入用户名: ", 60)
-            if username and verify_user_password(username, "", htpasswd_file):
-                change_user_password(username, htpasswd_file, manual=(choice == '4'))
+            while True:
+                username = input_with_timeout("请输入用户名: ", 60)
+                if username == "@":
+                    break
+                if user_exists(username, htpasswd_file):
+                    password = input_with_timeout("请输入密码: ", 60)
+                    if password == "@":
+                        break
+                    if verify_user_password(username, password, htpasswd_file):
+                        change_user_password(username, htpasswd_file, manual=(choice == '4'))
+                        break
+                    else:
+                        print("\033[91m密码错误。请重试。\033[0m")
+                else:
+                    print("\033[91m用户不存在。请重试。\033[0m")
 
         elif choice == '@':
             print("退出程序。")
